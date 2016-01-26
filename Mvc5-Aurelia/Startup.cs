48e1de07d577f1a3975944f877f6a3f602621be6
Rui Mourato <rui.mourato@gmail.com>
2016-01-26 09:25:58 +0000

    Mvc5 Aurelia Skeleton

---
+++ Mvc5-Aurelia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5_Aurelia.Startup))]
+namespace Mvc5_Aurelia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
